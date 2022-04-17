2803daca2e7d84622b7d30fca68a4282d18bc195
Martin Tamareu <mg.tamareu@gmail.com>
2022-04-17 02:09:39 -0300

    Se crea User Schema y la ruta para crear un user nuevo

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/user');
 const port = 3000;
 
-// Server Up
+// Server & DB Up
 const app = express();
-app.listen(3000, () => console.log(`Server running on port ${port}`));
+mongoose.connect('mongodb://localhost:27017/LogIn-Users').then(() => {
+  console.log('Connected Successfuly to DB');
+  app.listen(3000, () => console.log(`Server running on port ${port}`));
+}).catch((err) => {
+  console.log('Error to connect to the DB', err);
+});
 
 // Server Static Files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 
 // Routes
 app.get('/', (req, res) => {
@@ -19,6 +27,18 @@ app.get('/about', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.sendFile('./views/register.html', { root: __dirname });
+});
+
+app.post('/create_user', (req, res) => {
+  const newUserData = req.body;
+  const newUser = new User(newUserData);
+
+  newUser.save().then((result) => {
+    console.log('User saved successfuly', result);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log('User cannot be save', err);
+  });
 });
 
 app.use((req, res) => {
