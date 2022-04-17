5988795c99924bd97b5fb3dacb0da81876d87d4e
Martin Tamareu <mg.tamareu@gmail.com>
2022-04-16 23:40:14 -0300

    Agrego estilos a las vistas

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const port = 3000;
 
+// Server Up
 const app = express();
-
 app.listen(3000, () => console.log(`Server running on port ${port}`));
 
+// Server Static Files
+app.use(express.static('public'));
 
 // Routes
-
 app.get('/', (req, res) => {
   res.sendFile('./views/index.html', { root: __dirname });
 });
