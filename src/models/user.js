e1ad41926af4eb1fe67a5d967caec8a189c8a504
Martin Tamareu <mg.tamareu@gmail.com>
2022-04-20 22:17:40 -0300

    Agrego validaciones al user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,24 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: true
+    required: [true, 'Enter an username'],
+    unique: true,
+    minlength: [5, 'The minimum length for username is 5 characters']
   },
   password: {
     type: String,
-    required: true
+    required: [true, 'Enter a password'],
+    minlength: [8, 'The minimum length for password is 8 characters']
   },
   email: {
     type: String,
-    required: true
+    required: [true, 'Enter an email'],
+    unique: true,
+    validate: [isEmail, 'Please, enter a valid email']
   }
 }, { timestamps: true });
 
